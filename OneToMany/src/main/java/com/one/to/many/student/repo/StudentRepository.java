9f8e77a2e823aa12c5fb78ba523eceb354a86366
Sridhar <108978214+srid5969@users.noreply.github.com>
2022-10-21 20:00:53 +0530

    delte

--- OneToMany/src/main/java/com/one/to/many/student/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.one.to.many.student.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.one.to.many.student.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-
-}
